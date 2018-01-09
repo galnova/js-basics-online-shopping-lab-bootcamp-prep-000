f014ca1eedd664e331b6ef156e93de5af5949a50
Keith Jeter <kjeter2001@yahoo.com>
2018-01-09 16:09:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  const price = Math.floor((Math.random() * 100));
+  const price = Math.floor((Math.random() * 100) + 1);
   cart.push({item: price});
   
   console.log(`${item} has been added to your cart.`);
